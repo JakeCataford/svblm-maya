a4515f93bac5ab68b3121c11dde35f16bc40ebb6
JakeCataford <JakeCataford@Gmail.com>
2014-04-21 16:54:01 -0400

    Adds description of included modules to the module files.

--- cop/cop.py
+++ cop/cop.py
@@ -1,6 +1,35 @@
 import maya.cmds as cmds
 import unittest
 
+"""
+Cop - Unit Testing Wrapper for Maya Scenes
+
+What:
+
+We love testing. It lets us have confidence in what we are shipping without
+having to get other reviewers to look into it. This module wraps a unit testing
+framework around maya, letting us identify simple common problems in an automated
+way.
+
+Here are some examples of things Cop could prevent:
+
+- Huge texture files (for a small scale object)
+- Polycount problems (way too many polygons)
+- Geometry problems (non-manifold Geometry, lamina faces)
+- Draw call hell (too many materials, too many meshes)
+
+All of these have simple solutions, but are easy to miss when exporting. Cop
+yells at you when your tests fail, so nobody will have these problems again.
+
+How:
+
+Add tests to cop/test/. They have to start with the word "test_" and contain a
+class that extends unittest.TestCase. all of the functions of that class will be
+run so make sure they all assert something (failing your tests if things aren't)
+right.
+
+"""
+
 def thisDirectory():
     path_to_file = cmds.pluginInfo("cop", query=True, path=True)
     path_components = path_to_file.split("/")
--- shipper/shipper.py
+++ shipper/shipper.py
@@ -1,6 +1,43 @@
 import maya.cmds as cmds
 import os
 
+"""
+Svblm Shipper - A 3D Asset Pipeline
+
+What:
+
+Shipper is a way to manage reviewing and shipping assets to production.
+It focuses on integrating tightly with maya and dropbox to ensure that
+files remain well organized and easy to navigate when they reach production.
+The local->stage->production approach means that assets get reviewed before
+they hit production, ensuring high quality
+
+How:
+
+Shipper integrates with a local dropbox install. A shared directory
+is set as the shipper root, from there, shipper ensures that two
+subfolders exist, stage and production. stage is where we store candidates,
+any models that need to be reviewed before they are available in unity.
+
+The format for storing is like this:
+stage/
+    model_name/
+        model.fbx
+        model.fbm
+        meta.json
+
+meta.json is the metadata file, it contains the real name of the file and
+any useful other information. (grouping, tags, notes). We will build an
+asset pallette plugin in unity to filter these assets by tag and view them
+in a logical way, similar to the UDK content browser.
+
+There is a browser for all staged files in this plugin, where you can open
+staged files and ship them to production. production is just another folder
+with identical structure to stage that we use to store production ready assets.
+
+"""
+
+
 def initializePlugin(maya_object):
     #check if shipper is set...
     if cmds.optionVar(exists="shipper_dir") == 0
